4bd563e9ca9f9cdcd627f7e6ce41721304953804
swapnil <sopiswarrinku77@gmail.com>
2022-05-17 13:12:32 -0700

    Added : welcome window for AddressBook

---
+++ AddressBookMainUC1.java
@@ -0,0 +1,8 @@
+package com.Bridgelabz;
+
+public class AddressBookMainUC1 {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to address book program");
+    }
+}
